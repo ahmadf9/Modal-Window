4647bf61cfba47b030eaae6bb128419d858e857a
Ahmad Faizal <ahmadfaizalarrosyid@gmail.com>
2024-09-04 16:20:26 +0700

    Refactoring code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Modal-Window
+
+<!-- Lecture: Working With Classes -->
+
+Jadi untuk rekap, jika Anda ingin menggunakan fungsi yang sama di beberapa Event Listener, maka Anda perlu menentukan fungsi itu sebagai
+fungsi yang terpisah, seperti di sini.
+
+- const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+  };
+
+Dan kemudian Anda dapat meneruskan fungsi itu sebagai argumen ke beberapa metode "Add Event Listener". Seperti yang kita lakukan di sini (closeModal).
+
+- btnCloseModal.addEventListener('click', closeModal);
+
+Dan itu karena class pada dasarnya memungkinkan kita untuk menggabungkan beberapa properti CSS hanya dalam satu, seperti sebuah wadah.
+
+Jadi setiap class berfungsi seperti wadah dengan banyak properti di dalamnya. Dan kemudian di sini, dengan menambahkan (add) dan menghapusnya (remove), pada dasarnya kita dapat mengaktifkan dan menonaktifkan gaya tertentu, semuanya secara bersamaan.
--- script.js
+++ script.js
@@ -6,21 +6,35 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal'); // Memilih semua elemen selector
 console.log(btnsOpenModal);
 
+// Membuat dry code. Dengan mengekspor fungsi ini ke fungsi baru.
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Membuat dry code. Dengan mengekspor fungsi ini ke fungsi baru.
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 // Mengulang NodeList dan mendapatkan text content ketiga tombolnya.
 for (let i = 0; i < btnsOpenModal.length; i++)
   // Untuk merespon event klik, kita akan melampirkan fungsi event handler ke elemen.
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', function () {
+// Ketika elemen ini (btnCloseModal/overay) di klik, kita ingin menutup modal.
+btnCloseModal.addEventListener('click', closeModal); // Definisikan fungsi
+overlay.addEventListener('click', closeModal);
+
+// Sebelum diubah ke fungsi close Modal
+/* btnCloseModal.addEventListener('click', function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+}); 
 
 overlay.addEventListener('click', function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+}); */
