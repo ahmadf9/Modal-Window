bd0f4cdf38ee203926a76c8567ad0cc89676b1cf
Ahmad Faizal <ahmadfaizalarrosyid@gmail.com>
2024-09-04 15:49:47 +0700

    Lecture: Working With Classes. Menambah fitur close modal & close overlay

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ console.log(btnsOpenModal);
 
 // Mengulang NodeList dan mendapatkan text content ketiga tombolnya.
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  // Untuk merespon event klik, kita akan melampirkan fungsi event handler ke elemen.
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
